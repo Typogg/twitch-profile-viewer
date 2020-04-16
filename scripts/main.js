3dd2e2d66d8293e6ef6d409dd904153fe046816f
Typo <typogg@protonmail.com>
2020-04-16 11:21:43 +0200

    implemented first version of background mover
     - has some bugs when hovering over other elements
     - resets pattern when animation ends looks bad when moving cursor

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 import { ClassApplier } from './modules/ClassApplier.js';
+import { BackgroundMover } from './modules/BackgroundMover.js';
 
 const inputFieldClassApplier = new ClassApplier(
   '.main__search-wrapper',
@@ -6,3 +7,10 @@ const inputFieldClassApplier = new ClassApplier(
 );
 inputFieldClassApplier.addClassApplier('has--box-shadow', 'focus');
 inputFieldClassApplier.addClassRemover('has--box-shadow', 'blur');
+
+const animatedBackground = new BackgroundMover(
+  '.main__background',
+  '.main__wrapper'
+);
+
+animatedBackground.addEventListener(0.1);
